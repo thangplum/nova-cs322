43ae7a2e01d7949d81d17a418e3d35ce842c1b3b
choudry467 <choudhry467@outlook.com>
2021-05-20 21:40:28 -0500

    added api for auth token

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render  # noqa
+from allauth.socialaccount.models import SocialToken
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from .models import User
 from .serializers import serializerProfile
+import json
 
 class profileViewSet(viewsets.ViewSet):
     def list(self, request):
@@ -18,4 +20,11 @@ class profileViewSet(viewsets.ViewSet):
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(status=status.HTTP_400_BAD_REQUEST)
-   
+    
+    def retrieve(self, request, pk=None):
+        
+        if (not request.user.is_staff) and pk == "1":
+            access_token = SocialToken.objects.get(account__user=request.user)
+            return Response(access_token.token)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
