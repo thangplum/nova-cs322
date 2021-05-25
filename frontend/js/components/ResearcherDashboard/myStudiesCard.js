c5b8074ebd00b8445ba879e29efb6479841acc5e
thangplum <thangplum@gmail.com>
2021-05-24 19:13:26 -0500

    Add my researches page

--- frontend/js/components/ResearcherDashboard/myStudiesCard.js
+++ frontend/js/components/ResearcherDashboard/myStudiesCard.js
@@ -2,17 +2,22 @@ import React from 'react';
 import { Card, ListGroup } from "react-bootstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit, faCalendarTimes, faCalendarCheck } from '@fortawesome/free-solid-svg-icons';
+import { Link, useRouteMatch } from 'react-router-dom';
 
 const myStudiesCard = () => {
-
+  const { path } = useRouteMatch();
+  //console.log(route);
   return (
     <Card className="db-card">
       <Card.Header id="card-header">My studies</Card.Header>
       <ListGroup variant="flush">
-        <ListGroup.Item>
-          <FontAwesomeIcon style={{ marginRight: '5px'}} icon={faEdit} />
-          View and edit your studies
-        </ListGroup.Item>
+        <Link to={`${path}/my-researches`} style={{ textDecoration: 'none' }}>
+          <ListGroup.Item>
+            <FontAwesomeIcon style={{ marginRight: '5px'}} icon={faEdit} />
+            View and edit your studies
+          </ListGroup.Item>
+        </Link>
+        
         <ListGroup.Item>
         <FontAwesomeIcon style={{ marginRight: '5px'}} icon={faCalendarCheck} />
           View, add or edit timeslots
--- frontend/js/pages/Researcher/MyResearches.js
+++ frontend/js/pages/Researcher/MyResearches.js
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card, Table, Button, Pagination } from 'react-bootstrap';
 import getCookieToken from '../../utils/getCookieToken';
 import axios from 'axios';
 
 const MyResearches = () => {
+  const [studies, setStudies] = useState([]);
+
   useEffect(() => {
     const fetchResearch = async () => {
       var csrftoken = getCookieToken('csrftoken');
@@ -15,11 +17,10 @@ const MyResearches = () => {
             'X-CSRFToken': csrftoken
           }
         })
-        console.log(res);
+        setStudies(res.data);
       } catch(err) {
         console.log(err);
       }
-      
     }
     
     fetchResearch();
@@ -33,7 +34,26 @@ const MyResearches = () => {
           </Card.Header>
           <Card.Body>
             <Table striped bordered hover>
-              
+              <thead>
+                <tr>
+                  <th style={{ width: '10%' }}>Index</th>
+                  <th>Research Information</th>
+                  <th style={{ width: '15%' }}>Edit</th>
+                </tr>
+              </thead>
+              <tbody>
+                {studies.map((study) => {
+                  return (
+                    <tr key={study.id}>
+                      <th>{study.id}</th>
+                      <th><h4>{study.studyName}</h4></th>
+                      <th>
+                        <Button style={{ width: '100%' }}>Edit</Button>
+                      </th>
+                    </tr>
+                  );
+                })}
+              </tbody>
             </Table>
           </Card.Body>
         </Card>
